aba01a41b28a634b75bdfde1aa132e5cf46ee0b2
Zulchumor Kurbonova <zinochka@pochta.ru>
2017-01-10 17:52:23 +0200

    First readme stage

---
+++ Program.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Program
+{
+      	public static void main(String[] args)
+      	{
+        	System.out.println("Hello");
+		System.out.println("Happy New Year!");
+		System.out.println("Marry Christmas");
+		Scanner scanner = new Scanner (System.in);
+
+	}
+}
